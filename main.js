d63aa5e8ffd3bff6d89dda0143cb8f1ab461afc6
M.Sabback <m.sabbackdev@gmail.com>
2026-06-03 23:45:56 +0300

    add task done length

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+function startListener() {
+  document.querySelector(".input").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      checkUserValue();
+      console.log("enter is ckicked");
+    }
+  });
+}
+
 function checkUserValue() {
-  const userValue = document.querySelector(".input").value;
-  if (userValue !== "" && userValue.length > 3) {
-    createTodo(userValue);
+  const userValue = document.querySelector(".input");
+
+  if (userValue.value !== "" && userValue.value.length > 3) {
+    createTodo(userValue.value);
     document.querySelector(".input").value = "";
   } else {
     showErrorMsg();
   }
 }
+
+function hh() {}
 
 function createTodo(value) {
   const todo = document.createElement("div");
@@ -29,27 +42,44 @@ function createTodo(value) {
 
 function addTask(task) {
   document.querySelector(".todo-list").appendChild(task);
+  document.querySelector(".input").focus();
 }
 
 function doneTasks() {
-  this.parentElement.classList.add("done");
+  if (this.parentElement.className === "todo done") {
+    this.parentElement.classList.remove("done");
+  } else {
+    this.parentElement.classList.add("done");
+  }
   showDoneTasks();
 }
 
 function deleteTask() {
-  this.parentElement.remove();
+  if (this.parentElement.className === "todo done") {
+    this.parentElement.classList.remove("done");
+    this.parentElement.remove();
+  } else {
+    this.parentElement.remove();
+  }
+  showDoneTasks();
 }
 
 function showDoneTasks() {
-  return document.querySelectorAll(".done").length;
+  document.querySelector(".done-length").textContent =
+    `task you have done: ${document.querySelectorAll(".done").length}`;
 }
 
 function showErrorMsg() {
   const errorMsg = (document.createElement("h2").textContent =
     "Enter task not an empty and bigger then 3 chracter");
-  const rootElem = document.querySelector(".errorMsg");
+  const rootElem = document.querySelector(".error-msg");
   rootElem.append(errorMsg);
   setTimeout(() => {
     rootElem.childNodes[rootElem.childNodes.length - 1].remove();
-  }, 1000);
+  }, 3000);
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  startListener();
+  showDoneTasks();
+});
